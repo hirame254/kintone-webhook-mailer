47253289d0986c2e0c7cf8f03c80817ea9845111
荻原倫子 <31560168+hirame254@users.noreply.github.com>
2020-01-24 08:45:09 +0900

    feat:メール送信

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,29 @@
 const functions = require('firebase-functions').region('asia-northeast1')
-const mailer = require('nodemailer')
+//const mailer = require('nodemailer')
 const kintoneJSSDK = require('@kintone/kintone-js-sdk')
 const Mustache = require('mustache')
 const config = require('./config.json')
+require('dotenv').config()
+const admin = require('firebase-admin')
+const sgMail = require('@sendgrid/mail')
+const mailer = require('nodemailer')
 
-const sendMail = async (smtpName, mailOptions) => {
-  const smtpOptions = config.smtpServers.find(server => server.name === smtpName)
-  if (!smtpOptions) throw new Error('No SMTP config')
-  const transporter = mailer.createTransport(smtpOptions)
-  return await transporter.sendMail(mailOptions)
-}
-
-const fillTemplate = (templateRecord, requestBody) => {
-  const view = Object.entries(requestBody.record).reduce(
+// const fillTemplateOriginal = (templateRecord, requestBody) => {
+//   const view = Object.entries(requestBody.record).reduce(
+//     (acc, [key, value]) => ({ ...acc, [key]: value.value }),
+//     {}
+//   )
+//   return Object.entries(templateRecord).reduce((acc, [key, value]) => {
+//     return typeof value.value === 'string'
+//       ? {
+//           ...acc,
+//           [key]: Mustache.render(value.value, view),
+//         }
+//       : acc
+//   }, {})
+// }
+const fillTemplate = (templateRecord, record) => {
+  const view = Object.entries(record).reduce(
     (acc, [key, value]) => ({ ...acc, [key]: value.value }),
     {}
   )
@@ -26,39 +37,48 @@ const fillTemplate = (templateRecord, requestBody) => {
   }, {})
 }
 
-const composeAndSendMail = async requestBody => {
-  const templateRecord = await fetchTemplateRecord()
-  const template = fillTemplate(templateRecord, requestBody)
-  const mailOptions = {
-    from: template.fromMailAddress,
-    to: template.toMailAddress,
-    subject: template.subject,
-    text: template.body,
-  }
-  return await sendMail('default', mailOptions)
-}
-
-const infoFields = ['info', 'accepted', 'rejected', 'response', 'messageId']
-const createLogInfoRecord = info =>
-  Object.entries({ ...info, info }).reduce(
-    (acc, [key, value]) =>
-      infoFields.includes(key) ? { ...acc, [key]: { value: JSON.stringify(value) } } : acc,
-    {}
+const composeMail = (record, templates) => {
+  const templateRecord = templates.find(
+    template =>
+      template.mallName.value === record.mallName.value &&
+      template.localGovernment.value === record.localGovernment.value
   )
-
-const logInfo = async info => {
-  const logApp = getAppConfig('log')
-  if (!logApp) throw new Error('No log app config')
-  const { id, apiToken } = logApp
-  if (!id || !apiToken) throw new Error('Missing log app info')
-  try {
-    const record = getKintoneRecord(apiToken)
-    await record.addRecord({ app: id, record: createLogInfoRecord(info) })
-  } catch (error) {
-    console.error(error)
-    throw new Error(error)
-  }
+  return fillTemplate(templateRecord, record)
 }
+
+const sendMailWithSg = async msg => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  sgMail.send(msg)
+}
+
+const sendMailToSmtp = async (smtpName, msg) => {
+  const smtpOptions = config.smtpServers.find(server => server.name === smtpName)
+  if (!smtpOptions) throw new Error('No SMTP config')
+  const transporter = mailer.createTransport(smtpOptions)
+  return await transporter.sendMail(msg)
+}
+
+// const infoFields = ['info', 'accepted', 'rejected', 'response', 'messageId']
+// const createLogInfoRecord = info =>
+//   Object.entries({ ...info, info }).reduce(
+//     (acc, [key, value]) =>
+//       infoFields.includes(key) ? { ...acc, [key]: { value: JSON.stringify(value) } } : acc,
+//     {}
+//   )
+
+// const logInfo = async info => {
+//   const logApp = getAppConfig('log')
+//   if (!logApp) throw new Error('No log app config')
+//   const { id, apiToken } = logApp
+//   if (!id || !apiToken) throw new Error('Missing log app info')
+//   try {
+//     const record = getKintoneRecord(apiToken)
+//     await record.addRecord({ app: id, record: createLogInfoRecord(info) })
+//   } catch (error) {
+//     console.error(error)
+//     throw new Error(error)
+//   }
+// }
 
 const getAppConfig = type => {
   return config.kintone.apps.find(app => app.type === type)
@@ -71,15 +91,34 @@ const getKintoneRecord = apiToken => {
   return new kintoneJSSDK.Record({ connection })
 }
 
-const fetchTemplateRecord = async () => {
-  const templateApp = getAppConfig('template')
-  if (!templateApp) throw new Error('No template app config')
-  const { id, apiToken } = templateApp
-  if (!id || !apiToken) throw new Error('Missing template app info')
+// const fetchTemplateRecord = async () => {
+//   const templateApp = getAppConfig('mailTemplate')
+//   if (!templateApp) throw new Error('No template app config')
+//   const { id, apiToken } = templateApp
+//   if (!id || !apiToken) throw new Error('Missing template app info')
+//   try {
+//     const record = getKintoneRecord(apiToken)
+//     const response = await record.getRecords({ app: id })
+//     return response.records[0] || { error: `No template record on app ${id}` }
+//   } catch (error) {
+//     console.error(error)
+//     throw new Error(error)
+//   }
+// }
+
+const fetchOrderControlRecordsWithQuery = async (query, fields) => {
+  const orderCtrlApp = getAppConfig('orderControl')
+  if (!orderCtrlApp) throw new Error('No orderControl app config')
+  const { id, apiToken } = orderCtrlApp
+  if (!id || !apiToken) throw new Error('Missing orderControl app info')
   try {
     const record = getKintoneRecord(apiToken)
-    const response = await record.getRecords({ app: id })
-    return response.records[0] || { error: `No template record on app ${id}` }
+    const response = await record.getRecords({
+      app: id,
+      query: query,
+      field: fields,
+    })
+    return response.records || { error: `No orderControl record on app ${id}` }
   } catch (error) {
     console.error(error)
     throw new Error(error)
@@ -97,9 +136,39 @@ const checkHookTypes = requestBody => {
     app: { id },
   } = requestBody
   return config.kintone.apps.find(
-    app => app.type === 'source' && app.id == id && app.types.includes(type)
+    app => app.type === 'orderControl' && app.id == id && app.types.includes(type)
   )
 }
+
+exports.receiveKintoneRecords = functions.https.onCall(async (data, context) => {
+  try {
+    admin.initializeApp()
+    if (!context.auth) {
+      // throw new functions.https.HttpsError(
+      //   'failed-precondition',
+      //   'The function must be called ' + 'while authenticated.'
+      // )
+    }
+    for (const record of data.records) {
+      const template = composeMail(record, data.mailTemplates)
+      const msg = {
+        to: template.toMailAddress,
+        from: template.fromMailAddress,
+        subject: template.subject,
+        text: template.body,
+      }
+      switch (template.smtpServer.value) {
+        case '楽天':
+          await sendMailToSmtp('rakuten', msg)
+          break
+        default:
+          await sendMailWithSg(msg)
+      }
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 exports.receiveHook = functions.https.onRequest(async (request, response) => {
   try {
@@ -116,9 +185,16 @@ exports.receiveHook = functions.https.onRequest(async (request, response) => {
       return
     }
 
-    const info = await composeAndSendMail(request.body)
-    await logInfo(info)
-    response.status(200).send(info)
+    const fetchedRecords = await fetchOrderControlRecordsWithQuery(
+      'thanksOrder in ("未") order by $id limit 5'
+    )
+    for (const record of fetchedRecords) {
+      //      const info = await composeAndSendMail(record)
+      // const info = await sendMailsWithSg(record)
+      // await logInfo(info)
+      console.info(record)
+    }
+    response.status(200).send('OK!')
   } catch (error) {
     console.error(error)
     response.status(400).send(error)
